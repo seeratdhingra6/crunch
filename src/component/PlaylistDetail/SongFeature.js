ddac90fe4f93840fc99e7840d4403df6f5690498
Seerat Arora <118955100+seeratdhingra6@users.noreply.github.com>
2024-03-26 00:44:07 +0530

    Update SongFeature.js

--- src/component/PlaylistDetail/SongFeature.js
+++ src/component/PlaylistDetail/SongFeature.js
@@ -1,7 +1,6 @@
 import React from "react";
 import "./style/songFeature.css";
 import songFeatureImg from "../LandingPage/asset/songFeature.png";
-import ProgressBar from "bootstrap-progress-bar";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const SongFeature = () => {
@@ -15,27 +14,21 @@ const SongFeature = () => {
         <p className="songFeature__mainHeading">Features</p>
         <div className="songFeature__featureList">
           <div className="songFeature__featureBox">
-            <ProgressBar width="90%" />
             <p>Danceable</p>
           </div>
           <div className="songFeature__featureBox">
-            <ProgressBar width="100%" />
             <p>Danceable</p>
           </div>
           <div className="songFeature__featureBox">
-            <ProgressBar width="100%" />
             <p>Danceable</p>
           </div>
           <div className="songFeature__featureBox">
-            <ProgressBar width="100%" />
             <p>Danceable</p>
           </div>
           <div className="songFeature__featureBox">
-            <ProgressBar width="100%" />
             <p>Danceable</p>
           </div>
           <div className="songFeature__featureBox">
-            <ProgressBar width="100%" />
             <p>Danceable</p>
           </div>
         </div>
